8a81560cbafefe705a605a83e14b7f0cc240daa5
Noha9900 <150599591+Noha9900@users.noreply.github.com>
2026-02-16 13:14:17 +0530

    Create mega_download.py

---
+++ bot/helper/mirror_leech_utils/download_utils/mega_download.py
@@ -0,0 +1,10 @@
+# ==========================================
+#             PROFESSOR-X EDITION
+#     Ultra-Speed & Stable Leech Engine
+# ==========================================
+
+from ...listeners.mega_listener import MegaAppListener
+
+async def add_mega_download(listener, path):
+    mega_listener = MegaAppListener(listener)
+    await mega_listener.download(path)
